e56360470d0de1803cad699ade42e385c708780e
Anatoli Nicolae <desk@anatolinicolae.com>
2020-02-19 17:49:27 +0100

    Fix .htaccess write on successful build
    Signed-off-by: Anatoli Nicolae <desk@anatolinicolae.com>

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,49 +1,50 @@
 const path = require('path');
-const { exists, writeFile, ensureDir } = require('fs-extra');
+const {exists, writeFile, ensureDir} = require('fs-extra');
 
 const getMetaRedirect = require('./getMetaRedirect');
 
 async function writeRedirectsFile(redirects, folder, pathPrefix) {
-  if (!redirects.length) return;
+    if (!redirects.length) return;
 
-  let outputLines = [];
+    let outputLines = [];
 
-  outputLines.push(`<IfModule mod_rewrite.c>`);
-  outputLines.push(`RewriteEngine On`);
+    outputLines.push(`<IfModule mod_rewrite.c>`);
+    outputLines.push(`RewriteEngine On`);
 
-  for (const redirect of redirects) {
-    const { fromPath, toPath } = redirect;
-    console.log(fromPath, toPath);
-    outputLines.push(getMetaRedirect(fromPath, toPath));
-  }
-
-  outputLines.push(`</IfModule>`);
-
-  const FILE_PATH = path.join(
-    folder,
-    '.htaccess'
-  );
-
-  const fileExists = await exists(FILE_PATH);
-  if (!fileExists) {
-    try {
-      await ensureDir(path.dirname(FILE_PATH));
-    } catch (err) {
-      // ignore if the directory already exists;
+    for (const redirect of redirects) {
+        const {fromPath, toPath} = redirect;
+        console.log(fromPath, toPath);
+        outputLines.push(getMetaRedirect(fromPath, toPath));
     }
 
-    await writeFile(FILE_PATH, outputLines.join("\n"));
-  }
+    outputLines.push(`</IfModule>`);
+
+    const FILE_PATH = path.join(
+        folder,
+        '.htaccess'
+    );
+
+    const fileExists = await exists(FILE_PATH);
+
+    if (!fileExists) {
+        try {
+            await ensureDir(path.dirname(FILE_PATH));
+        } catch (err) {
+            // ignore if the directory already exists;
+        }
+    }
+
+    await writeFile(FILE_PATH, outputLines.join(`\n`));
 }
 
-exports.onPostBuild = ({ store }) => {
-  const { redirects, program, config } = store.getState();
+exports.onPostBuild = ({store}) => {
+    const {redirects, program, config} = store.getState();
 
-  let pathPrefix = '';
-  if (program.prefixPaths) {
-    pathPrefix = config.pathPrefix;
-  }
+    let pathPrefix = '';
+    if (program.prefixPaths) {
+        pathPrefix = config.pathPrefix;
+    }
 
-  const folder = path.join(program.directory, 'public');
-  return writeRedirectsFile(redirects, folder, pathPrefix);
+    const folder = path.join(program.directory, `public`);
+    return writeRedirectsFile(redirects, folder, pathPrefix);
 };
--- getMetaRedirect.js
+++ getMetaRedirect.js
@@ -1,18 +1,19 @@
 module.exports = function getMetaRedirect(fromPath, toPath) {
-  let url = toPath.trim();
+    let url = toPath.trim();
 
-  const hasProtocol = url.includes('://');
-  if (!hasProtocol) {
-    const hasLeadingSlash = url.startsWith('/');
-    if (!hasLeadingSlash) {
-      url = `/${url}`;
+    const hasProtocol = url.includes('://');
+
+    if (!hasProtocol) {
+        const hasLeadingSlash = url.startsWith('/');
+        if (!hasLeadingSlash) {
+            url = `/${url}`;
+        }
+
+        const resemblesFile = url.includes('.');
+        if (!resemblesFile) {
+            url = `${url}/`.replace(/\/\/+/g, '/');
+        }
     }
 
-    const resemblesFile = url.includes('.');
-    if (!resemblesFile) {
-      url = `${url}/`.replace(/\/\/+/g, '/');
-    }
-  }
-
-  return `RewriteRule ^${fromPath}/?$ ${url} [R=301,L]`;
+    return `RewriteRule ^${fromPath}/?$ ${url} [R=301,L]`;
 };
